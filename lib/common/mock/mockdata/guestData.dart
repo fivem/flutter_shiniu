392b1688c51dcfff63a7f791f0388e4ed1e13114
guanhao <guanhao@zhongwang.com>
2019-12-04 13:50:23 +0800

    update guest page

---
+++ lib/common/mock/mockdata/guestData.dart
@@ -0,0 +1,52 @@
+List data = [
+  {
+    "name":"宏伟集团",
+    "img":"assets/hongwei.jpg",
+    "content":"考察工厂, 调研产品。",
+    "number":"人数 3",
+    "inTime":"来访时间 2019-12-20 08:00:00",
+    "offTime":"离开时间 2019-12-20 17:00:00"
+  },{
+    "name":"华润万家",
+    "img":"assets/wanguard.jpg",
+    "content":"考察工厂, 探讨合作。",
+    "number":"人数 4",
+    "inTime":"来访时间 2019-12-23 08:00:00",
+    "offTime":"离开时间 2019-12-23 17:00:00"
+  },  {
+    "name":"友嘉超市",
+    "img":"assets/youjia.jpg",
+    "content":"考察工厂, 探讨合作。",
+    "number":"人数 1",
+    "inTime":"来访时间 2019-12-23 12:00:00",
+    "offTime":"离开时间 2019-12-23 17:00:00"
+  },{
+    "name":"大润发超市",
+    "img":"assets/rt-mart.jpg",
+    "content":"考察工厂, 调研产品。 ",
+    "number":"人数 2",
+    "inTime":"来访时间 2019-12-25 09:00:00",
+    "offTime":"离开时间 2019-12-25 12:00:00"
+  },{
+    "name":"友辉超市",
+    "img":"assets/yonghui.jpg",
+    "content":"考察工厂, 探讨合作。 ",
+    "number":"人数 3",
+    "inTime":"来访时间 2019-12-26 09:00:00",
+    "offTime":"离开时间 2019-12-26 13:00:00"
+  },{
+    "name":"京客隆",
+    "img":"assets/jingkelong.jpg",
+    "content":"考察工厂, 探讨合作。 ",
+    "number":"人数 4",
+    "inTime":"来访时间 2019-12-28 09:00:00",
+    "offTime":"离开时间 2019-12-28 15:00:00"
+  },{
+    "name":"好又多超市",
+    "img":"assets/trust-mart.jpg",
+    "content":"考察工厂, 调研产品。 ",
+    "number":"人数 1",
+    "inTime":"来访时间 2019-12-30 09:00:00",
+    "offTime":"离开时间 2019-12-30 11:00:00"
+  },
+];
