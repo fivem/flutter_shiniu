4304960f764d61fa3ce6b2411bac952805f75770
fivem <forever113@126.com>
2019-12-14 21:44:13 +0800

    add Switch

--- lib/main/producePage/productDetailPage/cowFormPage.dart
+++ lib/main/producePage/productDetailPage/cowFormPage.dart
@@ -21,7 +21,7 @@ class _CowFormPageState extends State<CowFormPage> {
   TextEditingController _EDCDateController = TextEditingController(text: formatDate(DateTime.now(), [yyyy, "-", mm, "-", dd]));
 
   CowEntity cowEntity = new CowEntity();
-
+  int _immuno = 0;
   @override
   Widget build(BuildContext context) {
 
@@ -33,18 +33,21 @@ class _CowFormPageState extends State<CowFormPage> {
           padding: EdgeInsets.symmetric(horizontal: 22.0),
           children: <Widget>[
              TextFormField(
-              decoration: InputDecoration(
+               enabled: widget.enable,
+               decoration: InputDecoration(
                 labelText: '编号',
-              ),
-              onSaved: (String value) => cowEntity.cowCode = value,
+               ),
+               onSaved: (String value) => cowEntity.cowCode = value,
             ),
             TextFormField(
+              enabled: widget.enable,
               decoration: InputDecoration(
                 labelText: '状态'
               ),
               onSaved: (String value) => cowEntity.state = value,
             ),
             TextFormField(
+              enabled: widget.enable,
               decoration: InputDecoration(
                 labelText: '周期'
               ),
@@ -53,47 +56,71 @@ class _CowFormPageState extends State<CowFormPage> {
                 print(cowEntity.birthDay);
               },
             ),
-             TextFormField(
-               keyboardType: TextInputType.number,
-               decoration: InputDecoration(
-                   labelText: '生产次数'
-               ),
-               onSaved: (String value) => cowEntity.period = value,
+            TextFormField(
+             enabled: widget.enable,
+             keyboardType: TextInputType.number,
+             decoration: InputDecoration(
+                 labelText: '生产次数'
              ),
-             //mark : InkWell is work but GestureDetector not work
-             _buildDateElement(_birthDayController, '出生日期', cowEntity.birthDay,(String date)=>cowEntity.birthDay = date),
+             onSaved: (String value) => cowEntity.period = value,
+            ),
+           //mark : InkWell is work but GestureDetector not work
+            _buildDateElement(_birthDayController, '出生日期', cowEntity.birthDay,(String date)=>cowEntity.birthDay = date),
 
-             _buildDateElement(_fertilizationDateController, '受精日期', cowEntity.fertilizationDate,(String date)=>cowEntity.fertilizationDate = date),
+            _buildDateElement(_fertilizationDateController, '受精日期', cowEntity.fertilizationDate,(String date)=>cowEntity.fertilizationDate = date),
 
-             _buildDateElement(_childbirthDateController, '分娩日期', cowEntity.childbirthDate,(String date)=>cowEntity.childbirthDate = date),
+            _buildDateElement(_childbirthDateController, '分娩日期', cowEntity.childbirthDate,(String date)=>cowEntity.childbirthDate = date),
 
-             _buildDateElement(_EDCDateController, '预产日期', cowEntity.EDC,(String date)=>cowEntity.EDC = date),
-
-             SizedBox(height: 60.0),
-             Align(
-               child:SizedBox(
-                 height: 45.0,
-                 width: 270.0,
-                 child: RaisedButton(
-                   child: Text(
-                     '保    存',
-                     style: Theme.of(context).primaryTextTheme.headline,
-                   ),
-                   color: Colors.blue,
-                   elevation:0,
-                   highlightElevation:0,
-                   disabledElevation:0,
-                     shape: StadiumBorder(side: BorderSide(color:Colors.blue)),
-                     onPressed: (){
-                     if(_formKey.currentState.validate()){
-                       _formKey.currentState.save();
+            _buildDateElement(_EDCDateController, '预产日期', cowEntity.EDC,(String date)=>cowEntity.EDC = date),
+            Row(
+             children: <Widget>[
+               Text('是否已经免疫'),
+               /*Radio(value: 1, groupValue: this._immuno, onChanged: (value)=>setState((){this._immuno = value;})),
+               Text("是"),
+               Radio(value: 0, groupValue: this._immuno, onChanged: (value)=>setState((){this._immuno = value;})),
+               Text("否"),*/
+               Expanded(
+                 child:Align(
+                 alignment: Alignment.centerRight,
+                 child: Switch(
+                   value: this._immuno==0?false:true,
+                   onChanged: (value){
+                     if(widget.enable!=false){
+                       setState(() {
+                         this._immuno = value?1:0;
+                       });
                      }
-
-                 }
-
+                   },
                  ),
                )
+               )
+
+             ],
+            ),
+
+            SizedBox(height: 15.0),
+            Align(
+             child:SizedBox(
+               height: 45.0,
+               width: 270.0,
+               child: RaisedButton(
+                 child: Text(
+                   '保    存',
+                   style: Theme.of(context).primaryTextTheme.headline,
+                 ),
+                 color: Colors.blue,
+                 elevation:0,
+                 highlightElevation:0,
+                 disabledElevation:0,
+                   shape: StadiumBorder(side: BorderSide(color:Colors.blue)),
+                   onPressed: (){
+                   if(_formKey.currentState.validate()){
+                     _formKey.currentState.save();
+                   }
+                 }
+               ),
              )
+           )
 
 
           ],
